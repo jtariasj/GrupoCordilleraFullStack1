bf2263ab98bf6890f6fb3aed79c05dc53c33c8e3
Jaimencio <jariasj@usm.cl>
2026-04-26 19:17:26 -0400

    Subidas las instrucciones

--- retail-service/src/main/java/com/grupocordillera/retail_service/RetailServiceApplication.java
+++ retail-service/src/main/java/com/grupocordillera/retail_service/RetailServiceApplication.java
@@ -9,5 +9,12 @@ public class RetailServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RetailServiceApplication.class, args);
 	}
-
+/*
+La empresa quiere desarrollar un sistema centralizado con los siguientes softwares:
+Sistemas de puntos de venta
+Plataforma de comercio electronico
+Sistemas de gestion de inventarios
+Herramientas de gestion financiera
+Sistema de atencion al cliente
+ */
 }
